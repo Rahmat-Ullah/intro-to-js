99903afdd0f6bd8a19aa512a80973bb263da73b7
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-10 13:58:44 +0600

    concatanation in JS practiced

---
+++ concatanation.js
@@ -0,0 +1,8 @@
+var price1 = 31;
+var price2 = 33;
+var TotalPrice = price1 + price2;
+
+var firstName = 'Jennie';
+var lastName= ' Kim';
+var fullName = firstName + lastName;
+console.log(fullName);
