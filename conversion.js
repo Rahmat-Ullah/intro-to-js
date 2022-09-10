bc53f62324fcd98a44f5beb4b3461de6a29c7fa8
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-10 14:00:34 +0600

    conversion in JS precticed

---
+++ conversion.js
@@ -0,0 +1,21 @@
+var price1 = 31;
+var price2 = 33;
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+
+var price1 = '31';
+var price2 = '33';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+//conver string to number
+console.log(price1);
+var numberPrice = parseInt(price1);
+console.log(numberPrice);
+
+
+var gpa = '3.04';
+console.log(gpa);
+var changeGpa = parseFloat(gpa);
+console.log(changeGpa);
