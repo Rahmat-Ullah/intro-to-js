cb7b48dab45b336926558c58cb47f7e7c47c3487
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-10 14:04:09 +0600

    some problem solved by js

---
+++ problemSolving.js
@@ -0,0 +1,34 @@
+//problem 1
+var mom = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+// console.log(totalPrice);
+console.log(mom-totalPrice);
+
+//problem 2
+
+var bangla = 75.25;
+var english = 65;
+var physics = 80;
+var math = 35.45;
+var chemestry = 99.50;
+
+var average = (bangla + english + physics + math + chemestry)/5;
+average = average.toFixed(2)
+average = parseFloat(average);
+console.log(average);
+
+//problem 3
+
+var firstString = 'I am going to be';
+var secondString = 'an awesome web developer';
+var fullString = firstString + ' ' + secondString;
+console.log(fullString);
+
+//problem 4
+
+var num = 119;
+var reminder = num % 5;
+console.log(reminder);
