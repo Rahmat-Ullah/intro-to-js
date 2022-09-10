f0fd33373435e2f638e25ae35ca5525dc706ede8
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-10 13:55:56 +0600

    operation in javascript practiced

---
+++ operation.js
@@ -0,0 +1,21 @@
+var onionPrice = 35;
+var eggPrice = 12;
+
+
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+// console.log(onionPrice);
+// console.log(eggPrice + onionPrice);
+
+var orange = 80;
+var quantity = 20;
+var totalCost = orange * quantity;
+console.log(totalCost);
+
+var money =4999;
+var worker = 37;
+var eacWorker = money / worker;
+console.log(eacWorker);
