1f1a07ea7aed68cfbc3bafe8fa6198a6501a2541
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-10 13:54:04 +0600

    variable practiced

---
+++ variable.js
@@ -0,0 +1,14 @@
+// numeric
+var price = 12;
+var age = 23;
+var gap = 5;
+var weight = 70;
+var work = 0;
+
+// string
+var name = "adu mia" ;
+var location = 'gazipur'
+
+// boolean
+var serious = true;
+var isFullMarks = true;
